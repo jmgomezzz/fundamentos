16edef33800ee02b1b66d01c9e1c9e9534664f41
jmgomezzz <jmanuelgomezruiz@gmail.com>
2025-10-21 11:22:22 +0000

    Factoria terminada

--- PRACTICA 1 FUNDAMENTOS/JavaScript.js
+++ PRACTICA 1 FUNDAMENTOS/JavaScript.js
@@ -45,12 +45,32 @@ class Factoria{
 subtipoPieza(){
     return arr[Math.floor(Math.random() * arr.length)];
     }
-//Los dígitos del código los generamos de una forma similar, usando el bucle for para 
+//Los dígitos del código los generamos de una forma similar, usando el bucle for para ir escribiendo los numeros
 digitoCodigo(){
     let s = ' ';
     for (let i = 0; i<n; i++){ 
     s += Math.floor(Math.random() * 10);
     }
     return s;
+}
+serialCompleto(isElecrtrica){
+    return this.digitoCodigo(10) +  (isElectrica ? 'E' : 'M');
+}
+//Fabricación de las piezas
+fabricacionPieza(Nombre = ' '){
+    const isElectrica = Math.random() < 0.3; //Así nos aseguramos de que haya un 30% de probabilidadaes de que salga electrica, luego pondremos con un else las mecanicas y ya
+    const fechaFabricación = new Date();
+    if (isElectrica){
+        const nombre = this.subtipoPieza(this.nombreElectricas);
+        const codigoPieza = this.serialCompleto(true);
+        const potencia = this.potencias[Math.floor(Math.random() * this.potencias.length)];
+        const voltaje = this.voltajes[Math.floor(Math.random() * this.voltajes.length)];
+        return new PiezaElectrica(nombre, codigoPieza, fechaFabricación, potencia, voltaje);
+    } else {
+        const nombre = this.subtipoPieza(this.nombreMecanicas);
+        const codigoPieza = this.serialCompleto(false);
+        const materialFabricación = this.materiales[Math.floor(Math.random() * this.materiales.length)];
+        return new PiezaMecanica(nombre, codigoPieza, fechaFabricación, materialFabricación);
+    }
 }
 }
