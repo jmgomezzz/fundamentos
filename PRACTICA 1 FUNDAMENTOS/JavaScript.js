75ee16a8cca3f4a4027485d4e618652a7b7968cb
Irenemendo114 <irenemendo114@gmail.com>
2025-10-21 11:06:09 +0100

    Add files via upload

---
+++ PRACTICA 1 FUNDAMENTOS/JavaScript.js
@@ -0,0 +1,12 @@
+
+<script>
+
+//Costructor objetos pieza
+function pieza(nombre, codigoPieza, fechaFabricación) {
+ this.nombre= nombre;
+ this.fechaFabricación=fechaFabricación;
+ this.codigoPieza= codigoPieza;
+    
+}
+
+</script>
